b5a4441ab7fe7597885af4011b607ab9d820fbea
Steffen Meyer <steffen@freiraum.xyz>
2021-06-29 13:39:27 +0200

    try

--- src/SnackbarContent/SnackbarContent.tsx
+++ src/SnackbarContent/SnackbarContent.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from 'react';
 import clsx from 'clsx';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/styles';
 import { SnackbarContentProps } from '../index';
 
 const styles = (theme: Theme) => createStyles({
@@ -12,13 +12,13 @@ const styles = (theme: Theme) => createStyles({
             flexGrow: 'initial',
             minWidth: 288,
         },
-    }
+    },
 });
 
 interface Props extends WithStyles<typeof styles>, SnackbarContentProps { }
 
 const SnackbarContent = forwardRef<HTMLDivElement, Props>(({ classes, className, ...props }, ref) => (
-    <div ref={ref} className={clsx(classes.root, className)}  {...props} />
-))
+    <div ref={ref} className={clsx(classes.root, className)} {...props} />
+));
 
 export default withStyles(styles)(SnackbarContent);
